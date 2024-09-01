1ac4250dd8bfa060621cd416ba570b7b275feb66
jaary5 <mujahid.jarif770@gmail.com>
2024-09-01 21:06:30 +0600

    Updated app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, abort
 import qrcode
 import os
 
@@ -7,27 +7,31 @@ app = Flask(__name__)
 # Ensure the 'images' directory exists
 os.makedirs(os.path.join('static', 'images'), exist_ok=True)
 
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         url = request.form.get("url")
         if url:
-            # Generate QR code
-            img = qrcode.make(url)
-            img_path = os.path.join('images', 'qrcode.png')  # Update the relative path
-            img.save(os.path.join('static', img_path))
+            try:
+                # Generate QR code
+                img = qrcode.make(url)
+                img_path = os.path.join('static', 'images', 'qrcode.png')
+                img.save(img_path)
 
-            return render_template("result.html", img_path=img_path)
+                return render_template("result.html", img_path='images/qrcode.png')
+            except Exception as e:
+                print(f"Error generating QR code: {e}")
+                abort(500)  # Internal Server Error
 
     return render_template("index.html")
-
 
 @app.route("/download")
 def download():
     path = os.path.join('static', 'images', 'qrcode.png')
-    return send_file(path, as_attachment=True)
-
+    if os.path.exists(path):
+        return send_file(path, as_attachment=True)
+    else:
+        abort(404)  # File not found
 
 if __name__ == "__main__":
     app.run(debug=True)
